24f7dc150903c33c0a31e1f2ad9664466a20c16d
jbarszcz <jan3k24@gmail.com>
2018-10-29 00:09:23 +0100

    Zadanie 3 i 4

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,12 +2,15 @@
 W wybranym języku programowania (C/C++/C#, java, python, javascript,:) wykonać:
 • Cztery działania na małych liczbach z wykorzystaniem procedur działania
 dokładnego dla wielkich liczb (p <100)
+
 • Cztery działania na dużych liczbach (dodawanie, odejmowanie, mnożenie,
 obliczanie odwrotności modulo p, 1024 bity)
+
 • Wykonanie szyfrowania i odszyfrowania pliku algorytmem symetrycznym
 o Klucze i pliki poprawne
 o Błąd w pliku zaszyfrowanym
 o Błąd w kluczu przy odszyfrowaniu
+
 • Obliczenie skrótu z pliku i sprawdzenie poprawności
 o Pliki poprawne
 o Plik uszkodzony/zmieniony 
@@ -18,17 +21,18 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
+import hashlib
 
-# opening for [r]eading as [b]inary
-file = open("file.txt", "rb")
 
-correct_data = file.read()
+def main():
+    data = read_file()
 
-file.close()
+    zadanie_3(data)
+    zadanie_4(data)
 
 
 def zadanie_3(correct_data):
-    print("Zadanie 3:\n")
+    print(">>> Zadanie 3 <<<:\n")
     print("Klucze i pliki poprawne:\n")
 
     message = correct_data
@@ -36,9 +40,11 @@ def zadanie_3(correct_data):
     padded_data = pad(message)
 
     good_key = os.urandom(32)
-    wrong_key = os.urandom(32)
+    wrong_key = corrupt(good_key)
 
-    print(f"Encrypting message {message}. After padding => {padded_data}")
+    # klucz i plik poprawne
+
+    print(f"Message {message} after padding: {padded_data}")
 
     encrypted_data = encrypt(padded_data, good_key)
     print(f"Encrypted data: {encrypted_data}")
@@ -47,8 +53,7 @@ def zadanie_3(correct_data):
     decrypted_message = unpad(decrypted_data)
     print(f"Message decrypted: {decrypted_data} => {decrypted_message}")
 
-    bad_decrypted_data = decrypt(encrypted_data, wrong_key)
-    print(f"Message decrypted with wrong key: {bad_decrypted_data}")
+    # niepoprawny plik
 
     print("\nBłąd w pliku zaszyfrowanym (pierwszy bajt zamieniony na 0): ")
 
@@ -56,6 +61,23 @@ def zadanie_3(correct_data):
     decrypted_corrupted_data = decrypt(corrupted_data, good_key)
 
     print(f"Decrypted corrupted data: {decrypted_corrupted_data}")
+
+    # niepoprawny klucz
+
+    print("\nBłąd w kluczu:")
+
+    bad_decrypted_data = decrypt(encrypted_data, wrong_key)
+    print(f"Message decrypted with wrong key: {bad_decrypted_data}")
+
+
+def zadanie_4(data):
+    print("\n>>> Zadanie 4 <<<:")
+
+    hash = generate_hash(data)
+    print(f"SHA256 hash of a file: {hash}")
+
+    hash = generate_hash(corrupt(data))
+    print(f"SHA256 hash of a corrupted file: {hash}")
 
 
 def pad(data):
@@ -74,27 +96,43 @@ def unpad(data):
 
 def corrupt(msg_bytes):  # zamienia pierwszy bajt na 0
     array = bytearray(msg_bytes)
-    array[0] = 0
+    if array[0] != 0:
+        array[0] = 0
+    else:
+        array[0] = 1  # jezeli 1 bajt byl wczesniej zerem to damy tam jedynke
+
     return bytes(array)
 
 
 def encrypt(data, key):
-    backend = default_backend()
     iv = b'\xd0\xbc\xbe\x80\x91\x19H\xf2[\x1b\xd3 \xb5\x85\xc7h'
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     encryptor = cipher.encryptor()
     ciphered_data = encryptor.update(data) + encryptor.finalize()
     return ciphered_data
 
 
 def decrypt(data, key):
-    backend = default_backend()
     iv = b'\xd0\xbc\xbe\x80\x91\x19H\xf2[\x1b\xd3 \xb5\x85\xc7h'
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
     decryptor = cipher.decryptor()
 
     decrypted_data = decryptor.update(data) + decryptor.finalize()
     return decrypted_data
 
 
-zadanie_3(correct_data)
+def generate_hash(data):
+    sha = hashlib.sha256()
+    sha.update(data)
+    return sha.hexdigest()
+
+
+def read_file():
+    file = open("file.txt", "rb")
+    data = file.read()
+    file.close()
+    return data
+
+
+if __name__ == "__main__":
+    main()
